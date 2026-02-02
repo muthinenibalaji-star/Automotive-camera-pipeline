dbf9cdb52cdce4f80bd36762008a8b4053da05dd
muthinenibalaji-star <muthinenibalaji@gmail.com>
2026-02-02 10:39:32 +0100

    Add visualization module init
    Add visualization module for real-time debug overlays.

---
+++ pipeline/visualization/__init__.py
@@ -0,0 +1,9 @@
+"""
+Visualization Module
+
+Provides real-time debug overlays for perception system.
+"""
+
+from .perception_visualizer import PerceptionVisualizer
+
+__all__ = ['PerceptionVisualizer']
